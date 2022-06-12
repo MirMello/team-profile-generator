b139a05615d418921931c00511e7612972fb5fed
MirMello <mirandaleeallen@gmail.com>
2022-06-12 10:11:00 -0600

    updated generateHTML with different stles

--- src/generateHTML.js
+++ src/generateHTML.js
@@ -5,13 +5,13 @@ const generateManager = function (manager) {
         <div class="card h-100">
             <div class="card-header">
                 <h3>${manager.name}</h3>
-                <h4>Manager</h4><i class="material-icons">content_paste</i>
+                <i class="material-icons">content_paste</i><h4>Manager</h4>
             </div>
 
             <div class="card-body">
-                <p class="id">ID: ${manager.id}</p>
-                <p class="email">Email: <a href="mailto:${manager.email}">${manager.email}</a></p>
-                <p class="office">Office Number: ${manager.officeNumber}</p>
+                <p class="id"><strong>ID:</strong> ${manager.id}</p>
+                <p class="email"><strong>Email:</strong> <a href="mailto:${manager.email}">${manager.email}</a></p>
+                <p class="office"><strong>Office Number:</strong> ${manager.officeNumber}</p>
             </div>
 
         </div>
@@ -26,13 +26,13 @@ const generateEngineer = function (engineer) {
         <div class="card h-100">
             <div class="card-header">
                 <h3>${engineer.name}</h3>
-                <h4>Engineer</h4><i class="material-icons">laptop_mac</i>
+                <i class="material-icons">laptop_mac</i><h4>Engineer</h4>
             </div>
 
             <div class="card-body">
-                <p class="id">ID: ${engineer.id}</p>
-                <p class="email">Email: <a href="mailto:${engineer.email}">${engineer.email}</a></p>
-                <p class="github">Github: <a href="https://github.com/${engineer.github}">${engineer.github}</a></p>
+                <p class="id"><strong>ID:</strong> ${engineer.id}</p>
+                <p class="email"><strong>Email:</strong> <a href="mailto:${engineer.email}">${engineer.email}</a></p>
+                <p class="github"><strong>Github:</strong> <a href="https://github.com/${engineer.github}">${engineer.github}</a></p>
             </div>
 
         </div>
@@ -47,13 +47,13 @@ const generateIntern = function (intern) {
         <div class="card h-100">
             <div class="card-header">
                 <h3>${intern.name}</h3>
-                <h4>Intern</h4><i class="material-icons">assignment_ind</i>
+                <i class="material-icons">assignment_ind</i><h4>Intern</h4>
             </div>
 
             <div class="card-body">
-                <p class="id">ID: ${intern.id}</p>
-                <p class="email">Email:<a href="mailto:${intern.email}">${intern.email}</a></p>
-                <p class="school">School: ${intern.school}</p>
+                <p class="id"><strong>ID:</strong> ${intern.id}</p>
+                <p class="email"><strong>Email:</strong><a href="mailto:${intern.email}">${intern.email}</a></p>
+                <p class="school"><strong>School:</strong> ${intern.school}</p>
             </div>
     </div>
 </div>
@@ -121,7 +121,7 @@ const generateTeamPage = function (employeeCards) {
   <body>
       <header>
           <nav class="navbar" id="navbar">
-              <span class="navbar-brand mb-0 h1 w-100 text-center" id="navbar-text">Team Profile</span>
+              <span class="navbar-brand mb-0 h1 w-100 text-center" id="navbar-text">My Team</span>
           </nav>
       </header>
       <main>
